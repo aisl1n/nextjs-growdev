e7b260729015b1f782c0f00e53a4f9055240f477
aisl1n <aislanarchy@gmail.com>
2024-02-21 00:29:26 -0300

    criado página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className='p-5 text-5xl font-black text-emerald-400'>
+        Seja bem vindo(a) ao meu espaço!✨
+      </h1>
+      <button className='bg-emerald-500 hover:bg-emerald-700 text-white font-bold mx-3 py-2 px-4 rounded-full shadow-md hover:shadow-lg transition duration-300 ease-in-out'>
+        <Link href='/sobre-mim'>Saiba mais sobre mim 🤗</Link>
+      </button>
     </>
   );
 }
